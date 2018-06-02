7ceb20a119355e4dbdcdecbecb020a061d834dce
dcc0 <dcc0@yandex.ru>
2018-06-02 20:40:38 +0300

    Update transliteration.c

--- .gitignore/transliteration.c
+++ .gitignore/transliteration.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 int main (int argc, char *argv[]) {
-    /*here we check arguments*/
+    /*Проверим аргументы. here we check arguments*/
     if (argc < 2) {
         printf ("Argument must be a char or string:\n");
         return 0;
@@ -29,18 +29,21 @@ int main (int argc, char *argv[]) {
  /*Читаем строку по одному символу до знака конца строки "\0". Here we are reading a string by character till "\0".  */
     while (argv[1][j]) {
         
-        /*Каждый четный цикл пишем в буфер. We write in the buffer
+        /*Каждый четный цикл пишем в буфер. We write in our buffer.
          each iteration, if 'j' is even number*/
         if (j%2 == 0) {
             c[0]=argv[1][j];
             c[1]=argv[1][j+1];
+            
             /*Запишем символ конца строки. We add  symbol of the end of a string.*/ 
             c[2]='\0';
             i = 0;
+            
             /*Сравним буфер и все строки массива. It compares
              buffer and strings in array*/
             while (i != 33) {
-                /*Проверим символ в массиве и напечатаем. Here we verify a character in  array and print it.*/
+                
+                /*Проверим символ в массиве и напечатаем. Here we verify a character in the array and print it.*/
                 if (strcmp(c, rus[i])==0)   {
                     printf ("%s", eng[i]);
                 }
