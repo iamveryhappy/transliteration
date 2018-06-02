d0a7a4e0f96634ce8870b2289e6eb80072d5425f
dcc0 <dcc0@yandex.ru>
2018-06-02 14:31:09 +0300

    Update transliteration.c

--- .gitignore/transliteration.c
+++ .gitignore/transliteration.c
@@ -25,18 +25,22 @@ int main (int argc, char *argv[]) {
     char c[4]; /*Буфер. Buffer*/
     int j = 0;
     int i = 0;
-
-    while (argv[1][j] != '\0') {
+    
+ /*Читаем строку по одному символу до знака конца строки "\0". Here we are reading a string by character till "\0".  */
+    while (argv[1][j]) {
+        
         /*Каждый четный цикл пишем в буфер. We write in the buffer
          each iteration, if 'j' is even number*/
         if (j%2 == 0) {
             c[0]=argv[1][j];
             c[1]=argv[1][j+1];
+            /*Запишем символ конца строки. We add  symbol of the end of a string.*/ 
             c[2]='\0';
             i = 0;
             /*Сравним буфер и все строки массива. It compares
              buffer and strings in array*/
             while (i != 33) {
+                /*Проверим символ в массиве и напечатаем. Here we verify a character in  array and print it.*/
                 if (strcmp(c, rus[i])==0)   {
                     printf ("%s", eng[i]);
                 }
