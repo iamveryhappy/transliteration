1aa0489a3a225bd03a2f22d6c54f6c726637bd4f
dcc0 <dcc0@yandex.ru>
2019-03-29 18:17:11 +0300

    Update low_to_upper_convert_rus_eng.c

--- low_to_upper_convert_rus_eng.c
+++ low_to_upper_convert_rus_eng.c
@@ -4,7 +4,7 @@
 #define ENGUPPERCASE  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
- /*Функция для английского символа*/
+  	/*Функция для английского символа*/
 	void eng_case (char eng) {
 
 		printf("%c",  eng);
@@ -12,7 +12,7 @@
 	}
 
 
- /*Функция для русской строки*/
+  	/*Функция для русской строки*/
 	void rus_case (char * rus) {
 
 	 char * rus_low[33] = { "а", "б", "в", "г", "д", "е",
@@ -21,7 +21,6 @@
 		"ч", "ш", "щ", "ь", "ы", "ъ", "э", "ю", "я"
                      };
 
-		
     char * rus_upper[33] = { "А", "Б", "В", "Г", "Д", "Е",
 		"Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н",
 		"О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц",
@@ -30,34 +29,33 @@
 
 
 
-         		char c[4]; /*Буфер. Buffer*/
-          		int j = 0;
-          		int i = 0;
+    char c[4]; /*Буфер. Buffer*/
+   		int j = 0;
+    		int i = 0;
 
 
-  /*Читаем строку по одному символу до знака конца строки "\0".
-  * Here we are reading a string by character till "\0".  */
+  	/*Читаем строку по одному символу до знака конца строки "\0".
+ 	 * Here we are reading a string by character till "\0".  */
     while (rus[j]) {
 
 
-        /*Каждый четный цикл пишем в буфер. We are writing into buffer.
-         each iteration, if 'j' is even number*/
-       if (j%2 == 0) {
-        	  	c[0]=rus[j];
-           		c[1]=rus[j+1];
+   	/*Каждый четный цикл пишем в буфер. We are writing into buffer.
+   	 each iteration, if 'j' is even number*/
+     	if (j%2 == 0) {
+     	 	c[0]=rus[j];
+      	 	c[1]=rus[j+1];
 
 
-        /*Запишем символ конца строки. We add  symbol of the end of a string.*/
+  	 /*Запишем символ конца строки. We add  symbol of the end of a string.*/
            c[2]='\0';
            i = 0;
 
-            /*Сравним буфер и все строки массива. It compares
-            buffer and strings in array*/
+   	/*Сравним буфер и все строки массива. It compares
+	    buffer and strings in array*/
             while (i != 33) {
 
-		    
-                /*Проверим символ в массиве и напечатаем.
-                 * Here we verify a character in the array and print it.*/
+   	 /*Проверим символ в массиве и напечатаем.
+    * Here we verify a character in the array and print it.*/
                 if (strcmp(c, rus_low[i])==0)   {
                     printf ("%s", rus_upper[i]);
                 }
@@ -74,12 +72,12 @@
 
 
 	{
-			if (argc < 2)
-			return 0;
+		if (argc < 2)
+		return 0;
 
-			int i = 0;
-			int j = 0;
-			int x = 0; /*Триггер*/
+		int i = 0;
+		int j = 0;
+		int x = 0; /*Триггер*/
 
 
 	for (j = 0; j != strlen(argv[1]); j++) {
@@ -89,8 +87,8 @@
 		for (i = 0; i != strlen(ENGLOWCASE); i++) {
 
 			if (argv[1][j] == ENGLOWCASE[i] || argv[1][0] == ENGUPPERCASE[i]) {
-			eng_case (ENGUPPERCASE[i]);  	/*Если английский символ*/
-			x = 1; 			       	/*Установим триггер в 1*/
+			eng_case (ENGUPPERCASE[i]); 	/*Если английский символ*/
+			x = 1; 				/*Установим триггер в 1*/
 			break;
 			}
 		}
